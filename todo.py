3bcddc54f06524d9ad46ec1c03e8b5547707d617
Burak Özlece <burakozleceplus@gmail.com>
2022-05-23 10:30:21 +0300

    edits

--- todo.py
+++ todo.py
@@ -1,9 +1,13 @@
 # pip install flask komutuyla indiriyoruz
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+
 # pip install flask_mysqldb komutuyla indiriyoruz
 from flask_mysqldb import MySQL
+
 # pip install passlib komutuyla indiriyoruz
 from passlib.hash import sha256_crypt
+
+# login required için kütüphane
 from functools import wraps 
 
 
@@ -34,6 +38,8 @@ app.secret_key = "sadasld02!"
 # Giriş Sayfası
 @app.route("/", methods = ["GET", "POST"])
 def index():
+
+
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -43,6 +49,7 @@ def index():
         result = cursor.execute(sorgu, (username,))
         data = cursor.fetchone()
         cursor.close()
+
         if(list_title):
             cursor = mysql.connection.cursor()
             sorgu2 = "SELECT * FROM users_todo_head WHERE userID = %s"
@@ -60,8 +67,10 @@ def index():
                     return redirect(url_for("detail", id = result2["todo_head_id"]))
                 else:
                     return render_template("index.html")
+            
             else:
                 return render_template("index.html")
+
         else:
             if (result) > 0:
                 if  sha256_crypt.verify(password, data["password"]): 
@@ -79,14 +88,19 @@ def index():
     else:
         return render_template("index.html")
 
+
 # Kontrol Paneli
 @app.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
+
+
     cursor = mysql.connection.cursor()
     sorgu = "SELECT * FROM users_todo_head JOIN todo_head ON users_todo_head.todo_head_id = todo_head.id WHERE userid = %s"
     sorgu2 = "SELECT todo_head.title, userID, users.username FROM `users_todo_head` JOIN users ON users_todo_head.userID = users.id JOIN todo_head ON users_todo_head.todo_head_id = todo_head.id WHERE todo_head_id IN (SELECT todo_head_id from users_todo_head WHERE userID = %s) and userID != %s"
     result = cursor.execute(sorgu,(session["id"],))
+
+
     if result > 0:
         todo = cursor.fetchall()
         resutl2 = cursor.execute(sorgu2,(session["id"],session["id"]))
@@ -121,16 +135,21 @@ def dashboard():
 @app.route("/todos/<string:id>", methods=["GET", "POST"])
 @login_required
 def detail(id):
+
+
     cursor = mysql.connection.cursor()
     sorgu = "SELECT * FROM todo JOIN todo_head ON todo.todo_head_id = todo_head.id WHERE todo_head_id = %s"
     sorgu2 = "SELECT * FROM users_todo_head JOIN todo_head ON users_todo_head.todo_head_id = todo_head.id WHERE userid = %s"
     result = cursor.execute(sorgu,(id,))
+
+
     if result > 0:
         todo = cursor.fetchall()
         cursor.execute(sorgu2,(session["id"],))
         todo2 = cursor.fetchall()
         cursor.close()
         return render_template("todos.html",todo = todo, todo2 = todo2)   
+
     else:
         sorgu3 = "DELETE FROM todo_head WHERE id = %s"
         cursor.execute(sorgu3,(id,))
@@ -146,6 +165,8 @@ def detail(id):
 @app.route("/del/<string:id>")
 @login_required
 def delete(id):
+
+
     cursor = mysql.connection.cursor()
     sorgu2 = "SELECT todo_head_id FROM todo where id = %s"
     cursor.execute(sorgu2,(id,))
@@ -162,28 +183,39 @@ def delete(id):
 @app.route("/edit/<string:id>")
 @login_required
 def edit(id):
+
+
     cursor = mysql.connection.cursor()
     sorgu = "SELECT todo_head_id FROM todo where id = %s"
     cursor.execute(sorgu,(id,))
     result = cursor.fetchone()
     result = result["todo_head_id"]
+
+
     sorgu2 = "SELECT complete FROM todo WHERE id = %s "
     cursor.execute(sorgu2,(id,))
     result2 = cursor.fetchone()
     result2 = result2["complete"]
     result2 = not(result2)
+
+
     sorgu3 = "UPDATE todo SET complete = %s WHERE id = %s"
     cursor.execute(sorgu3,(result2,id))
     mysql.connection.commit()
     cursor.close()
     return redirect(url_for("detail", id = result))
  
+
+
 # Görev ve Kullanıcı Ekle
 @app.route("/addTask/<string:id>", methods=["GET","POST"])
 @login_required
 def addTask(id):
+
+
     usernameAndTask = request.form.get("usernameAndTask")
     cursor = mysql.connection.cursor()
+
     if(usernameAndTask.startswith("@")):
         usernameAndTask = usernameAndTask.split(",")
         for i in usernameAndTask:
@@ -192,10 +224,12 @@ def addTask(id):
             cursor.execute(sorgu,(x,))
             result = cursor.fetchone()
             result = result["id"]
+
             sorgu2 = "INSERT INTO users_todo_head(userID, todo_head_id) VALUES(%s,%s)"
             cursor.execute(sorgu2,(result,id))
             mysql.connection.commit()
     else:
+
         sorgu = "INSERT INTO todo(todo_head_id, content, complete) VALUES(%s,%s, 0)"
         cursor.execute(sorgu,(id,usernameAndTask))
         mysql.connection.commit()
@@ -229,24 +263,34 @@ def register():
 # Çıkış yapma
 @app.route("/logout")
 def logout():
+
     session.clear()
     return redirect(url_for("index"))
+
+
 
 
 # Şifre sıfırlama
 @app.route("/forget", methods = ["GET","POST"])
 def forget():
+
+
     if request.method == "POST":
+
         username = request.form.get("username")
         name = request.form.get("name")
         lastname = request.form.get("lastname")
         email = request.form.get("email")
+
+
         sorgu = "SELECT * FROM users WHERE username = %s AND name = %s AND lastname = %s AND email = %s"
         cursor = mysql.connection.cursor()
         result = cursor.execute(sorgu,(username,name,lastname,email))
         data = cursor.fetchone()
+
         session["userPassword"] = username
-        print(session["userPassword"])
+
+
         if(result > 0):
             if (data["password"] == ""):
                 return render_template("forget.html", message=2)
@@ -262,12 +306,15 @@ def forget():
 @app.route("/newPassword", methods = ["GET","POST"])
 def newPassword():
     if request.method == "POST":
+
         newPassword = request.form.get("new_password")
         newPassword_confirm = request.form.get("new_confirm")
+
         if newPassword == newPassword_confirm:
-            print(session["userPassword"])
+
             crypt_password = sha256_crypt.encrypt(newPassword)
             cursor = mysql.connection.cursor()
+
             sorgu = "UPDATE users SET password = %s WHERE username = %s"
             cursor.execute(sorgu,(crypt_password,session["userPassword"]))
             mysql.connection.commit()
@@ -285,6 +332,7 @@ def newlist():
     sorgu = "SELECT * FROM users_todo_head JOIN todo_head ON users_todo_head.todo_head_id = todo_head.id WHERE userid = %s"
     result = cursor.execute(sorgu,(session["id"],))
     todo = cursor.fetchall()
+    
     if request.method == "POST":
         title = request.form.get("title")
         kategori = request.form.get("category")
